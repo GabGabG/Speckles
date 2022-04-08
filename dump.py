ba9e0e43ff7a2b05da8a8ef9604c10b2450ceb95
Gabriel Genest <9.genest.gabriel@gmail.com>
2022-04-08 14:15:10 -0400

    dumb tests

--- dump.py
+++ dump.py
@@ -6,6 +6,8 @@ import time
 from matplotlib.animation import ArtistAnimation, FuncAnimation
 import scipy.stats as stats
 from scipy.optimize import curve_fit
+from scipy.signal import convolve2d, convolve
+from scipy.ndimage import gaussian_filter
 
 plt.rcParams['animation.ffmpeg_path'] = r'C:\Users\goubi\ffmpeg-5.0-essentials_build\bin\ffmpeg.exe'
 
@@ -16,25 +18,7 @@ def cov(X, Y):
     return np.mean((X - np.mean(X)) * (Y - np.mean(Y)))
 
 
-if __name__ == '__main__':
-    shape = 10_000
-    vals = stats.expon.rvs(2, size=shape)
-    vals_2, bins, _ = plt.hist(vals, int(len(vals) ** .5), density=True)
-    x = np.linspace(np.min(vals), np.max(vals), int(len(vals) ** .5))
-    stuff = stats.expon.fit(vals)
-    beta = stuff[-1]
-    f = lambda x_, beta_: 1 / beta_ * np.exp(-x / beta_)
-    residuals = vals_2 - f(x, beta)
-    ss_res = np.sum(residuals ** 2)
-    ss_tot = np.sum((vals_2 - np.mean(vals_2)) ** 2)
-    r_squared = 1 - (ss_res / ss_tot)
-    print(r_squared)
-    lambda_ = 1 / beta
-    plt.plot(x, lambda_ * np.exp(-lambda_ * x), color="black")
-    plt.show()
-    stuff = stats.ks_1samp(vals, lambda x_: 1 - np.exp(-x / beta))
-    print(stuff)
-    exit()
+def moving_pupil():
     shape = (1000, 1000)
     radius = 50
     Y, X = np.indices(shape)
@@ -73,26 +57,23 @@ if __name__ == '__main__':
     plt.scatter(pos_x / (radius * 2), rho_s, label="Coefficients de corrélation des speckles")
     msg = "Coefficients de corrélation (au carré) du mouvement de la pupille"
     plt.plot(pos_x / (radius * 2), np.power(rho_masks, 2), color="red", label=msg)
-    # bessel = special.b
     plt.xlabel(r"Pupil position lag normalized to pupil diameter")
     plt.ylabel(r"Correlation coefficient $\rho(I_0, I_\tau)$")
     plt.legend()
     plt.show()
-    exit()
+
+
+def decorrelation_following_function():
     nb = 50
     shape = (600, 600)
-    tau_s = np.linspace(0, 1.85, nb)
-    tau_c = 0.37
-    rho_s = np.exp(-tau_s / tau_c)
-    r_s = np.sqrt(rho_s)
     x = np.linspace(0, 2 * np.pi, nb)
-    r_s = ((np.sin(x) + 1) / 2)
+    r_s = np.linspace(0, 1, 50)[::-1]
     plt.plot(x, r_s)
     plt.show()
     M1 = np.exp(-1j * np.random.uniform(-np.pi, np.pi, shape))
     M2 = np.exp(-1j * np.random.uniform(-np.pi, np.pi, shape))
-    r_s_mat = np.ones((*shape, nb))
-    r_s_mat[150:451, ...] = np.multiply.outer(r_s_mat[150:451, :, 0], r_s)
+    r_s_mat = np.ones(shape)
+    r_s_mat = np.multiply.outer(r_s_mat, r_s)
     M1 = np.broadcast_to(M1, r_s_mat.transpose((2, 0, 1)).shape).transpose((1, 2, 0))
     M2 = np.broadcast_to(M2, r_s_mat.transpose((2, 0, 1)).shape).transpose((1, 2, 0))
     W = r_s_mat * M1 + np.sqrt(1 - r_s_mat ** 2) * M2
@@ -100,7 +81,7 @@ if __name__ == '__main__':
     Y -= shape[0] // 2
     X -= shape[0] // 2
     mask = (X ** 2 + Y ** 2 - 25 ** 2) <= 0
-    f = lambda matrix: np.abs(np.fft.ifft2(np.fft.fft2(matrix) * mask)) ** 2
+    f = lambda matrix: np.abs(np.fft.ifft2(np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(matrix)) * mask))) ** 2
     I_s = [f(W[:, :, i]).real for i in range(nb)]
     I_s = [I_s[i] / np.max(I_s[i]) for i in range(len(I_s))]
     fig, ax = plt.subplots()
@@ -110,16 +91,19 @@ if __name__ == '__main__':
     plt.show()
     rho_s = []
     for i in range(nb):
-        X = I_s[0][150:451, ...]
-        Y = I_s[i][150:451, ...]
+        X = I_s[0]
+        Y = I_s[i]
         rho = cov(X, Y) / (np.std(X) * np.std(Y))
         rho_s.append(rho)
-    plt.scatter(range(nb), rho_s)
-    plt.xlabel(r"Step")
+    rho_s = np.array(rho_s)
+    plt.scatter(r_s, rho_s, color="red", marker="x")
+    plt.xlabel(r"Correlation coefficient between initial phase matrices $r$")
     plt.ylabel(r"Correlation coefficient $\rho(I_0, I_\tau)$")
-    # plt.plot(range(nb), 1 - r_s ** 0.5)
+    plt.plot(r_s, r_s ** 2)
     plt.show()
-    exit()
+
+
+def brownian():
     tau_s = np.arange(0, 3, 0.037)
     tau_c = 0.8
     rho_s_th = np.exp(-tau_s / tau_c)
@@ -170,40 +154,100 @@ if __name__ == '__main__':
     plt.ylabel(r"Correlation coefficient $\rho(I_0, I_\tau)$")
     plt.show()
 
-    exit()
-    nFrames = 175
-    dists = [np.random.normal(0, 2, 1000 * 1000) for _ in range(nFrames)]
-    fig, ax = plt.subplots()
-    n, bins, patches = ax.hist(dists[0], 256)
-    global previous_max_y, previous_max_x, previous_min_x
-    previous_max_y = max(n)
-    previous_max_x = max(bins)
-    previous_min_x = min(bins)
-    ax.set_ylim(top=previous_max_y)
-    ax.set_xlim(previous_min_x - 0.5 * np.abs(previous_min_x), previous_max_x + 0.5 * np.abs(previous_min_x))
-    ax.set_title(f"Histogram of time step {0}")
+
+def divide_chunk(array, n):
+    return [np.mean(array[i:i + n, :, :], axis=0) for i in range(0, array.shape[0], n)]
 
 
-    def update_hist(i):
-        ax.clear()
-        n, bins, patches = ax.hist(dists[i], 256)
-
-        current_max_y = max(n)
-        current_max_x = max(bins)
-        current_min_x = min(bins)
-        global previous_max_y, previous_max_x, previous_min_x
-        if previous_max_y < current_max_y:
-            previous_max_y = current_max_y
-        if current_max_x > previous_max_x:
-            previous_max_x = current_max_x
-        if current_min_x < previous_min_x:
-            previous_min_x = current_min_x
-        ax.set_ylim(top=previous_max_y)
-        ax.set_xlim(previous_min_x - 0.5 * np.abs(previous_min_x), previous_max_x + 0.5 * np.abs(previous_min_x))
-        ax.set_title(f"Histogram of time step {i}")
-        return patches  # .patches
+def speckling(mask: np.ndarray):
+    phases = np.random.uniform(-np.pi, np.pi, mask.shape)
+    phasors = np.exp(1j * phases)
+    speckles = (np.abs(np.fft.fftshift(np.fft.fft2(phasors * mask))) ** 2).real
+    speckles /= np.max(speckles)
+    return speckles
 
 
-    anim = FuncAnimation(fig, update_hist, frames=nFrames, blit=False, repeat=False)
+def gamma(x, k, scale):
+    return stats.gamma.pdf(x, k, scale=scale)
+
+
+def exponential_fit(data):
+    return stats.expon.fit(data, floc=0)
+
+
+def gamma_fit(data):
+    return stats.gamma.fit(data, floc=0)
+
+
+def gamma_cdf(x, n, loc, theta):
+    return stats.gamma.cdf(x, n, scale=theta, loc=loc)
+
+
+def exponential_cdf(x, loc, scale):
+    return stats.expon.cdf(x, scale=scale, loc=loc)
+
+
+def gamma_pdf(x, n, loc, theta):
+    return stats.gamma.pdf(x, n, scale=theta, loc=loc)
+
+
+def exponential_pdf(x, loc, scale):
+    return stats.expon.pdf(x, loc=loc, scale=scale)
+
+
+def speckling_speckles(mask, n_speckles):
+    speckles = np.full((*mask.shape, n_speckles), np.nan)
+    means = []
+    for i in range(n_speckles):
+        current_speckles = speckling(mask)
+        speckles[:, :, i] = current_speckles
+        means.append(np.mean(current_speckles))
+    s_speckles = np.sum(speckles, axis=-1)
+    return s_speckles, means
+
+
+if __name__ == '__main__':
+    n = 1
+    shape = (1000, 1000)
+    Y, X = np.indices(shape)
+    Y -= shape[0] // 2
+    X -= shape[1] // 2
+    mask = (X ** 2 + Y ** 2 - 100 ** 2) <= 0
+    s_speckles, means = speckling_speckles(mask, n)
+    data = s_speckles.ravel()
+    args_gamma = gamma_fit(data)
+    args_expon = exponential_fit(data)
+    n_data, bins, _ = plt.hist(data, 256, None, True)
+    x = (bins[:-1] + bins[1:]) / 2
+    plt.plot(x, gamma_pdf(x, *args_gamma), color="green", linestyle="--", label="Gamma fit")
+    plt.plot(x, exponential_pdf(x, *args_expon), color="red", linestyle=":", label="Exponential fit")
+    plt.legend()
+    print(args_gamma)
+    res_gamma = stats.ks_1samp(data, gamma_cdf, args_gamma)
+    print(f"K-S Test avec gamma : {res_gamma}")
+    print(args_expon)
+    res_expon = stats.ks_1samp(data, exponential_cdf, args_expon)
+    print(f"K-S Test avec exponentielle : {res_expon}")
     plt.show()
-    anim.save("test_bar.mp4")
+    exit()
+    n = 10
+    shape = (1000, 1000)
+    Y, X = np.indices(shape)
+    Y -= shape[0] // 2
+    X -= shape[1] // 2
+    mask = (X ** 2 + Y ** 2 - 100 ** 2) <= 0
+    speckles = np.full((*mask.shape, n), np.nan)
+    means = []
+    for i in range(n):
+        current_speckles = speckling(mask)
+        speckles[:, :, i] = current_speckles
+        means.append(np.mean(current_speckles))
+    print(means)
+    scale = np.mean(means)
+    s_speckles = np.mean(speckles, axis=-1)
+    plt.imshow(s_speckles, cmap="gray")
+    plt.show()
+    n_, bins, _ = plt.hist(s_speckles.ravel(), bins=256, density=True)
+    x = (bins[:-1] + bins[1:]) / 2
+    plt.plot(x, gamma(x, n, 1 / n * scale), color="red", linestyle="--")
+    plt.show()
